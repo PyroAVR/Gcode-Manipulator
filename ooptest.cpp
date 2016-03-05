a8fef7330ca6a32ad7a25f526e3a9b2d0f637d89
Andy Meyer <itechahm@me.com>
2016-03-04 19:24:34 -0500

    Fully functional OOP structure.

--- ooptest.cpp
+++
@@ -1,9 +0,0 @@
-#include "RS274.hpp"
-using namespace std;
-int main(int argc, char const *argv[]) {
-  RS274 g;
-  g.parse("test.ngc");
-  g.shift(10,5,-2.53);
-  g.write("out.ngc");
-  return 0;
-}
