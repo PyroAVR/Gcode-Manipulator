4258b0f33dfddb67f4f36470450d808e9b839a05
Andy Meyer <itechahm@me.com>
2016-04-21 20:29:51 -0400

    working on possible bug where lines go missing.

--- tests/debug_info.cpp
+++ tests/debug_info.cpp
@@ -1,15 +1,45 @@
 #include "../RS274.hpp"
 #include <thread>
-#include <mutex>
+#include <string>
 #include <chrono>
 #include <cstdlib>
 
+std::string prompt(std::string question, std::string prompt) {
+  std::cout << question << prompt;
+  std::string response;
+  std::cin >> response;
+  std::cout << std::endl;
+  return response;
+}
+
+
 int main(int argc, char* argv[])  {
   if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " <input> " << std::endl;
+    std::cout << "Usage: " << argv[0] << " <input> [start] [end]" << std::endl;
     return -1;
   }
-  RS274 *g = new RS274(argv[1], "");
-  std::cout << "initialized" << std::endl;
-  std::cout << g->run() << std::endl;
+  std::cout << "Working... please wait.";
+  threadWorkerData tw;
+  std::ifstream input;
+  std::string line;
+  tw.id = 0;
+  input.open(argv[1], std::fstream::in);
+  if(argc < 4)  {
+    while(!input.eof() && !input.bad()) {
+      std::getline(input, line, '\n');
+      tw.lines.push_back(line);
+    }
+  } else  {
+    input.seekg(atoi(argv[2]));
+    while(!input.eof() && !input.bad() && tw.lines.size() <= atoi(argv[3])) {
+      std::getline(input, line, '\n');
+      tw.lines.push_back(line);
+    }
+  }
+  RS274Worker *rw = new RS274Worker(tw);
+  rw->run();
+  tw = rw->getParsedData();
+  std::cout << " Done!" << std::endl;
+  std::cout << tw.instructionMatrix.size() << std::endl;
+
 }
